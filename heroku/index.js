d5ecb1e093189f860685b207a04150cd5fb5d6c3
Anand Akula <30412802+Githubhappy@users.noreply.github.com>
2020-09-22 17:38:28 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -26,6 +26,7 @@ app.get('/', function(req, res) {
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
+  console.log("req----",req);
   if (
     req.query['hub.mode'] == 'subscribe' &&
     req.query['hub.verify_token'] == token
